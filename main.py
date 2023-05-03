09e2bacd12688df699c3b0a6d0f97e90c698f64a
전강현 <aniren@debian.moe>
2023-05-04 00:23:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ file_list = natsort.natsorted(os.listdir(path))
 
 num = 1
 for fileName in file_list:
-    #change file name to 1.mp4, 2.mp4, 3.mp4 ...
-    os.rename(f'{path}\{fileName}', f'{path}\{num}.mp4')
+    #change file name to 1.ts, 2.ts, 3.ts ...
+    os.rename(f'{path}\{fileName}', f'{path}\{num}.ts')
     #use ffmpeg (re_encoding)
-    os.system(f'ffmpeg -i "{path}\{num}.mp4" -vcodec copy -acodec copy "{path}\{fileName}".mp4')
+    newfileName = fileName.replace(".ts", ".mp4")
+    os.system(f'ffmpeg -i "{path}\{num}.ts" -vcodec copy -acodec copy "{path}\{newfileName}"')
     num += 1
 num -= 1
 print("\n\n")
 for i in range(num):
-    #delete 1.mp4, 2.mp4, 3.mp4 ...
-    os.remove(f'{path}\{i + 1}.mp4')
-    print(f'Delete | {path}\{i + 1}.mp4')
+    #delete 1.ts, 2.ts, 3.ts ...
+    os.remove(f'{path}\{i + 1}.ts')
+    print(f'Delete | {path}\{i + 1}.ts')
+print("\n\n")
